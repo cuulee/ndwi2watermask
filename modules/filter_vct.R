d77e851311972508c51ce1cd7b488edbf75c23a8
jmigueldelgado <jose.martins.delgado@uni-potsdam.de>
2017-11-30 17:14:29 +0100

    joins scene with cogerh, gets ids, ingestion date, saves

--- modules/filter_vct.R
+++ modules/filter_vct.R
@@ -2,8 +2,8 @@ require(sf)
 require(dplyr)
 library(lubridate)
 library(rgeos)
-
-orson=TRUE
+library(reshape2)
+orson=FALSE
 
 if(orson)
 {
@@ -22,6 +22,10 @@ wmIn <- paste0(scratch,"/watermasks")
 
 flist <- list.files(wmIn,pattern=".gml")
 
+cogerh <- st_read(paste0(proj,"/auxdata/cogerh/cogerh.gml")) %>% as_tibble %>% st_as_sf() %>% st_set_crs(32724) %>% select(-fid)
+
+#st_write(cogerh,paste0(proj,"/auxdata/cogerh/cogerh.gml"))
+
 ### read, remove small parts, remove DN==0 (land), simplify with threshold between 10 and 15 preserving topology. It should reduce size of vector by a factor of at least 3.
 for(f in flist)
     {
@@ -35,10 +39,15 @@ for(f in flist)
             filter(as.numeric(area)>1000) %>%
             select(-fid,-DN)
         
-        psimpl <- st_simplify(p,preserveTopology=TRUE,dTolerance=11) %>%
-            st_transform(crs=4326) ## back to latlong
+        psimpl <- st_simplify(p,preserveTopology=TRUE,dTolerance=11)
+        ints <- st_intersects(psimpl,cogerh,sparse=TRUE) %>% melt(.)
 
+        ids=data_frame(id_cogerh=cogerh$id[ints$value],id_in_scene=psimpl$id_in_scene[ints$L1])
         
-        st_write(psimpl,paste0(wmIn,"/",f,"_simplified.gml"),driver="GML")
-        if(file.exists(paste0(wmIn,"/",f))) file.remove(paste0(wmIn,"/",f))
+        #pfilter <- left_join(ids,psimpl) %>% st_as_sf %>% split(.$id_cogerh) %>% lapply(st_union) %>% do.call(c,.) %>% st_cast
+        pfilter <- left_join(ids,psimpl) %>% st_as_sf %>% group_by(id_cogerh) %>% summarize(ingestion_time=first(ingestion_time),area=sum(area)) %>%
+            st_transform(crs=4326) ## back to latlong
+        
+        st_write(pfilter,paste0(wmIn,"/",f,"_simplified.gml"),driver="GML")
+        #if(file.exists(paste0(wmIn,"/",f))) file.remove(paste0(wmIn,"/",f))
     }
