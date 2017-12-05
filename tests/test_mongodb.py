bfdfd5339ebdeb18cbabaee19cc67558636c1e3c
jmigueldelgado <jose.martins.delgado@uni-potsdam.de>
2017-12-05 13:50:16 +0100

    testing pymongo

--- tests/test_mongodb.py
+++ tests/test_mongodb.py
@@ -4,22 +4,69 @@ from datetime import datetime
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client.sar2watermask
-
-with open('/home/delgado/scratch/watermasks/S1A_IW_GRDH_1SDV_20171124T080913_20171124T080938_019404_020E8C_F3CA_x12864_y0_watermask_simplified.geojson') as f:
-    data = json.load(f)
-
 s2w = db.sar2watermask
+
+orson=False
+if(orson):
+    pyt="/users/stud09/martinsd/local/miniconda2/envs/gdal/bin/python"
+    gdalPol="/users/stud09/martinsd/local/miniconda2/envs/gdal/bin/gdal_polygonize.py"
+    proj="/users/stud09/martinsd/proj/sar2watermask"
+    scratch="/mnt/scratch/martinsd"
+else:
+    pyt="/home/delgado/local/miniconda2/bin/python2"
+    gdalPol="/home/delgado/local/miniconda2/bin/gdal_polygonize.py"
+    proj="/home/delgado/proj/sar2watermask"
+    scratch="/home/delgado/scratch"
+
     
-for feat in data["features"]:
-    print(feat["properties"])
-    dttm = datetime.strptime(feat["properties"]["ingestion_time"],"%Y/%m/%d %H:%M:%S+00")
-    feat["properties"]["ingestion_time"] = dttm
-    feat_id = s2w.insert_one(feat).inserted_id
-    print feat_id
+sardir=scratch+"/s1a_scenes"
+sarIn=sardir+"/in"
+sarOut=sardir+"/out"
+polOut=scratch + "/watermasks"
+items=os.listdir(polOut)
+
+newlist = []
+
+for names in items:
+    if names.endswith('simplified.geojson'):
+        newlist.append(names)
+
+for in_file in newlist:
+    print('\n inserting ' + in_file + ' in mongodb\n')
+
+    with open(polOut + '/' + in_file) as f:
+        data = json.load(f)
+
+    
+    for feat in data["features"]:
+        dttm = datetime.strptime(feat["properties"]["ingestion_time"],"%Y/%m/%d %H:%M:%S+00")
+        feat["properties"]["ingestion_time"] = dttm
+        feat_id = s2w.insert_one(feat).inserted_id
+        print feat_id
 
 import pprint
-d = datetime(2017, 11, 1, 0)
+d2 = datetime(2017, 11, 28, 0)
+d1 = datetime(2017, 11, 25, 0)
 
-print s2w.find_one({'properties.ingestion_time' : {'$gte': d}})
+q2 = s2w.find({'properties.ingestion_time' : {'$gte': d2}})
+q1 = s2w.find({'properties.ingestion_time' : {'$lte': d1}})
+q3 = s2w.find({'properties.id_cogerh' : 3359})
+
+## aggregate NOT WORKING YET!!!
+
+pipeline = [
+    { "$sort" : {"properties.id_cogerh" : 1, "properties.ingestion_time" : 1 }},
+    {
+        "$group":
+        {
+            "id_cogerh" : "properties.id_cogerh",
+            "lastPolygon": {"$last":"$properties.ingestion_time"}
+        }
+    }
+]
+
+p=s2w.aggregate(pipeline)
+
+print q2[0]["geometry"]["type"]
 print s2w.count({'properties.id_cogerh' : 3359})
 print s2w.count({'properties.ingestion_time' : {'$gte': d}})
