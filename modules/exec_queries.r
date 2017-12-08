6c00d7f47de6cc516d64ff8aa270dc3329fee8bb
Jose Delgado <jmigueldelgado@gmail.com>
2017-12-08 16:16:04 +0100

    testing query from mongodb to sf

--- modules/exec_queries.r
+++ modules/exec_queries.r
@@ -2,6 +2,7 @@ library(mongolite)
 library(sf)
 library(tidyr)
 library(dplyr)
+library(jsonlite)
 
 m <- mongo("sar2watermask","sar2watermask","mongodb://localhost:27017/")
 
@@ -29,4 +30,24 @@ pipeline = '[
 
 polys=m$aggregate(pipeline,'{"allowDiskUse":true}')
 
-poly = m$find('{"properties.id_cogerh" : 26969}')
+for(i in 1:10])
+{ 
+    poly = m$find(paste0('{"properties.id_cogerh" : ',polys$`_id`[i],'}'))
+    if(poly$geometry$type=="MultiPolygon")
+    {
+        for(j in 1:poly$geometry$coordinates[[1]])
+        {
+            latlong[[j]] <- rbind(poly$geometry$coordinates[[1]][[i]][,,1],poly$geometry$coordinates[[1]][[i]][,,2]) %>% t
+        }
+        sfclist[[i]] <- lapply(latlong,st_multipolygon)
+    } else if(poly$geometry$type=="Polygon")
+    {
+        latlong <- rbind(poly$geometry$coordinates[[1]][,,1],poly$geometry$coordinates[[1]][,,2]) %>% t
+    }
+}
+
+poly = m$find('{"properties.id_cogerh" : 26946}')
+
+sfgPl <- st_polygon(list(latlong)))
+sfcPl <- st_geometry(sfgPl)
+sfPl <- st_sf(sfcPl,crs=4326) %>% bind_cols(.,poly$properties)
