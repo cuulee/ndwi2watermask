bb9ef4ba4082791a11744057846e865281f1b873
Dr. rer. nat. José Miguel Martins Delgado <jose.martins.delgado@uni-potsdam.de>
2018-01-08 17:39:03 +0100

    teste shiny app com leaflet. mapa com poligonos funciona mas e' mt grande

--- tests/app.r
+++ tests/app.r
@@ -1,2 +1,38 @@
-library(rsconnect)
-rsconnect::deployApp('path/to/your/app')
+library(shiny)
+library(leaflet)
+library(rgdal)
+library(raster)
+
+poly <- st_read("~/proj/sar2watermask/tests/0_latest.geojson")
+
+poly <- poly["geometry"]
+
+                                        #rasterize
+
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
+
+ui <- fluidPage(
+  leafletOutput("mymap"),
+  p()#,
+ # actionButton("recalc", "New points")
+)
+
+server <- function(input, output, session) {
+
+    
+#    points <- eventReactive(input$recalc, {
+#        cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+#    }, ignoreNULL = FALSE)
+    
+  output$mymap <- renderLeaflet({
+    leaflet(poly) %>%
+      addProviderTiles(providers$Stamen.TonerLite,
+        options = providerTileOptions(noWrap = TRUE)
+        ) %>%
+        addPolygons()
+#      addMarkers(data = points())
+  })
+}
+
+shinyApp(ui, server)
