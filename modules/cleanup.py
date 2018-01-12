dbf027637ddccc72f053f24aee0cf5884691ab3f
Dr. rer. nat. José Miguel Martins Delgado <jose.martins.delgado@uni-potsdam.de>
2018-01-12 14:49:34 +0100

    corrects cleanup with full paths

--- modules/cleanup.py
+++ modules/cleanup.py
@@ -5,19 +5,19 @@ from getPaths import *
 current_time = time.time()
 
 for f in os.listdir(sarOut):
-    creation_time = os.path.getctime(f)
+    creation_time = os.path.getctime(satOut + "/" + f)
     if (current_time - creation_time) // (24 * 3600) >= 14:
-        os.unlink(f)
+        os.unlink(satOut + "/" +f)
         print('{} removed'.format(f))
 
 for f in os.listdir(sarIn):
-    creation_time = os.path.getctime(f)
+    creation_time = os.path.getctime(satIn + "/" +f)
     if (current_time - creation_time) // (24 * 3600) >= 14:
-        os.unlink(f)
+        os.unlink(satIn + "/" +)
         print('{} removed'.format(f))
 
 for f in os.listdir(polOut):
-    creation_time = os.path.getctime(f)
+    creation_time = os.path.getctime(polOut + "/" + f)
     if (current_time - creation_time) // (24 * 3600) >= 14:
-        os.unlink(f)
+        os.unlink(polOut + "/" +f)
         print('{} removed'.format(f))
