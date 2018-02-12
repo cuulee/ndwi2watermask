8c5d589ca3c30e18c8c4f8cce32d637b3f8b6572
JM Delgado <jmigueldelgado@gmail.com>
2018-02-12 15:30:26 +0100

    creates conda environment image.yml

--- modules/thresholding_ndwi.py
+++ modules/thresholding_ndwi.py
@@ -6,17 +6,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import subprocess
 from skimage.filters import threshold_otsu, threshold_local
-file_clouds = '/home/delgado/proj/sar2watermask/cloud.img'
-p3 = '/home/delgado/Downloads/S2A_MSIL1C_20170728T130251_N0205_R095_T24MUV_20170728T130248.SAFE/GRANULE/L1C_T24MUV_A010960_20170728T130248/IMG_DATA/T24MUV_20170728T130251_B03.jp2'
-p8 = '/home/delgado/Downloads/S2A_MSIL1C_20170728T130251_N0205_R095_T24MUV_20170728T130248.SAFE/GRANULE/L1C_T24MUV_A010960_20170728T130248/IMG_DATA/T24MUV_20170728T130251_B08.jp2'
-
+file_clouds = '/home/delgado/scratch/s2a_scenes/in/S2A_MSIL1C_20170728T130251_N0205_R095_T24MUV_20170728T130248.SAFE/GRANULE/L1C_T24MUV_A010960_20170728T130248/IMG_DATA/cloud.img'
+p3 = '/home/delgado/scratch/s2a_scenes/in/S2A_MSIL1C_20170728T130251_N0205_R095_T24MUV_20170728T130248.SAFE/GRANULE/L1C_T24MUV_A010960_20170728T130248/IMG_DATA/T24MUV_20170728T130251_B03.jp2'
+p8 = '/home/delgado/scratch/s2a_scenes/in/S2A_MSIL1C_20170728T130251_N0205_R095_T24MUV_20170728T130248.SAFE/GRANULE/L1C_T24MUV_A010960_20170728T130248/IMG_DATA/T24MUV_20170728T130251_B08.jp2'
 
 
 dataset_clouds = rio.open(file_clouds)
 clouds = dataset_clouds.read(1)
-
-clouds10 = np.empty(shape=(round(clouds.shape[0] * 2),
-                         round(clouds.shape[1] * 2)))
+clouds10 = np.empty(shape=(int(round(clouds.shape[0] * 2)),
+                         int(round(clouds.shape[1] * 2))))
 
 from affine import Affine
 aff = dataset_clouds.transform
@@ -37,24 +35,21 @@ clouds_bin = (clouds10==2) | (clouds10==3)
 plt.imshow(clouds_bin)
 
 
-transx=5000
-transy=5000
-h=5000
-w=5000
+#transx=5000
+#transy=5000
+#h=5000
+#w=5000
 
 
 dataset3 = rio.open(p3)
-band3 = dataset3.read(1,window=Window(transx,transy,w,h))
-
-#dataset = rio.open(p3)
-#band = dataset.read(1,window=Window(transx,transy,100,100))
+band3 = dataset3.read(1)
+#band3 = dataset3.read(1,window=Window(transx,transy,w,h))
 
 dataset8 = rio.open(p8)
-band8 = dataset8.read(1,window=Window(transx,transy,w,h))
-
+band8 = dataset8.read(1)
 
 profile = dataset3.profile
-profile.update(dtype=rio.uint8,count=1,height=h,width=w,transform=profile['transform']*Affine.translation(transx,transy))
+profile.update(dtype=rio.uint8,count=1)#,height=h,width=w,transform=profile['transform']*Affine.translation(transx,transy))
 
 #fileName='example-ndwi_dummy'
 
@@ -62,19 +57,13 @@ profile.update(dtype=rio.uint8,count=1,height=h,width=w,transform=profile['trans
 #    dst.write(band.astype(rio.uint8), 1)
 
 NDWI = (band3-band8)/(band3+band8)
-
-np.shape(NDWI)
-np.shape(band3)
-np.shape(band8)
-
-
-
 NDWI.max()
-ndwi = NDWI>50
+
+###### should be between 0 and 1 !!!
+ndwi = NDWI[not clouds_bin]>0.5
 
 
 plt.imshow(ndwi.astype(rio.uint8))
-
 
 fileName='example-ndwi_manual'
 
