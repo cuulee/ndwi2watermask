76e1799f73ff5881a5c914462499f20eb8c02c0a
JM Delgado <jmigueldelgado@gmail.com>
2018-02-20 11:20:52 +0100

    structuring project with __main__ and __init__

---
+++ __main__.py
@@ -0,0 +1,17 @@
+import sys
+import bin.ndwi2watermask as n2w
+
+def main(args=None):
+    """The main routine."""
+    if args is None:
+        args = sys.argv[1:]
+
+    try:
+        n2w.ndwi2watermask()
+        return 0
+    except:
+        return 1
+
+
+if __name__ == "__main__":
+    main()
--- bin/ndwi2watermask.py
+++ bin/ndwi2watermask.py
@@ -1,15 +1,34 @@
 import os
-import modules.getPaths as pths
+import modules.getpaths as pths
 import numpy as np
-from modules.defCloudMask import unzipJp2, getBandDir,interpolate_clouds_to_10m
+from modules.cloudmask import unzipJp2, getBandDir,runGdalbuildvrt,runFmaskMakeAngles,runFmaskStack
 from modules.ndwi import ndwi_from_jp2
+import re
 
-zipfls=[]
-items=os.listdir(pths.s2aIn)
 
-for item in items:
-    item=pths.s2aIn + '/' + item
-    if re.search('^.*\.zip$', item) :
-        zipfls.append(item)
-        sceneJp2 = unzipJp2(item)
-        ndwi_from_jp2(sceneJp2)
+def ndwi2watermask():
+    print("Executing ndwi2watermask():")
+    zipfls=[]
+    items=os.listdir(pths.s2aIn)
+
+    for item in items:
+        item=pths.s2aIn + '/' + item
+        if re.search('^.*\.zip$', item) :
+            zipfls.append(item)
+            sceneJp2 = unzipJp2(item)
+            ndwi_from_jp2(sceneJp2)
+
+
+def rmclouds():
+    print("Executing rmclouds():")
+    zipfls=[]
+    items=os.listdir(pths.s2aIn)
+
+    for item in items:
+        item=pths.s2aIn + '/' + item
+        if re.search('^.*\.zip$', item) :
+            zipfls.append(item)
+            sceneJp2 = unzipJp2(item)
+            runGdalbuildvrt(sceneJp2)
+            runFmaskMakeAngles(sceneJp2)
+            runFmaskStack(sceneJp2)
