4c6259a87101b6c3194e7b8043937b4c817600c8
jmigueldelgado <jmigueldelgado@gmail.com>
2018-02-21 13:24:32 +0100

    clean old files

--- tests/test_rw_cluster.sh
+++
@@ -1,3 +0,0 @@
-export JAVA_TOOL_OPTIONS="-Xmx12g"
-export _JAVA_OPTIONS="-Xmx14g"
-/users/stud09/martinsd/local/miniconda2/bin/python2 /users/stud09/martinsd/proj/sar2watermask/modules/test_rw_cluster.py
