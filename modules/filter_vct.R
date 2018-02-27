aa278bef42b9d58c39372412bd34614ed730c8f7
JM Delgado <jmigueldelgado@gmail.com>
2018-02-27 11:28:10 +0100

    creates jobs and adapts st_simplify to include sentinel 2

--- modules/filter_vct.R
+++ modules/filter_vct.R
@@ -2,28 +2,20 @@ require(sf)
 require(dplyr)
 library(lubridate)
 library(reshape2)
-orson=FALSE
-
 orson=TRUE
-
 if(orson)
 {
-    proj="/users/stud09/martinsd/proj/sar2watermask"
     scratch="/mnt/scratch/martinsd"
 } else
 {
-    proj="/home/delgado/proj/sar2watermask"
     scratch="/home/delgado/scratch"
 }
-    
-sardir=paste0(scratch,"/s1a_scenes")
-sarIn=paste0(sardir,"/in")
-sarOut=paste0(sardir,"/out")
+
 wmIn <- paste0(scratch,"/watermasks")
 
 flist <- list.files(wmIn,pattern="watermask.gml$")
 
-cogerh <- st_read(paste0(proj,"/auxdata/cogerh.geojson")) %>%
+cogerh <- st_read("./parameters/cogerh.geojson") %>%
     as_tibble %>%
     st_as_sf() %>%
     st_set_crs(32724)
@@ -48,17 +40,31 @@ for(f in flist)
         if(nrow(p)>0)
         {
             p <- p %>% st_transform(crs=32724) %>%
-                mutate(ingestion_time=strsplit(f,"_")[[1]][5] %>%
-                           ymd_hms()) %>%
                 mutate(id_in_scene=row_number(),area=st_area(.)) %>%
                 filter(as.numeric(area)>1000) %>%
                 select(-fid,-DN)
-            
+            if(strsplit(f,"_")[[1]][1]==S1A)
+                {
+                  cat("simplifying S1A")
+                  p = p %>%
+                 mutate(ingestion_time=strsplit(f,"_")[[1]][5] %>%
+                 ymd_hms(),platformname='Sentinel-1')
+               }
+            if(strsplit(f,"_")[[1]][1]==S2A)
+               {
+                 cat("simplifying S2A")
+                 p = p %>%
+                 mutate(ingestion_time=strsplit(f,"_")[[1]][3] %>%
+                 ymd_hms(),platformname='Sentinel-2')
+               }
+
+
+
             psimpl <- st_simplify(p,preserveTopology=TRUE,dTolerance=11)
             ints <- st_intersects(psimpl,cogerh,sparse=TRUE) %>% unclass(.) %>% melt(.)
-            
+
             ids=data_frame(id_cogerh=cogerh$id[ints$value],id_in_scene=psimpl$id_in_scene[ints$L1])
-            
+
                                         #pfilter <- left_join(ids,psimpl) %>% st_as_sf %>% split(.$id_cogerh) %>% lapply(st_union) %>% do.call(c,.) %>% st_cast
             if(nrow(ids)>0)
             {
