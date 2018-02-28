ad194a8c4b8e66306b67b3e03d36801f779484db
JM Delgado <jmigueldelgado@gmail.com>
2018-02-28 21:00:51 +0100

    checking if rmcloud has already run. if so, skip step

--- modules/ndwi.py
+++ modules/ndwi.py
@@ -48,12 +48,10 @@ def ndwi_from_jp2(sceneJp2):
 
 def ndwi2watermask():
     print("Executing ndwi2watermask():")
-    zipfls=[]
     items=os.listdir(pths.s2aIn)
 
     for item in items:
         item=pths.s2aIn + '/' + item
         if re.search('^.*\.zip$', item) :
-            zipfls.append(item)
             sceneJp2 = unzipJp2(item)
             ndwi_from_jp2(sceneJp2)
