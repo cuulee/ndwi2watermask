560bcc5eb508faef7e0a911c76f68b55d3705dfe
JM Delgado <jmigueldelgado@gmail.com>
2018-02-28 10:52:29 +0100

    reorganizes _main_ so it does not import any modules before knowing which script to process

--- __main__.py
+++ __main__.py
@@ -11,33 +11,29 @@ def main():
 
     if sys.argv[1] is None:
         print("an argument is needed, for example: cleanup or get_scenes")
+    elif sys.argv[1] is "getscenes":
+        import modules.scenes as scenes
+        try:
+            scenes.getscenes()
+            return 0
+        except:
+            return 1
+    elif sys.argv[1] is "rmclouds":
+        import modules.cloudmask as clouds
+        try:
+            clouds.rmclouds()
+            return 0
+        except:
+            return 1
+    elif sys.argv[1] is "ndwi":
+        import modules.ndwi as n2w
+        try:
+            n2w.ndwi2watermask()
+            return 0
+        except:
+            return 1
     else:
-        switcher.get(sys.argv[1],'please provide one of rmclouds, getscenes or ndwi')()
-
-def callgetscenes():
-    import modules.scenes as scenes
-    try:
-        scenes.getscenes()
-        return 0
-    except:
-        return 1
-
-def callrmclouds():
-    import modules.cloudmask as clouds
-    try:
-        clouds.rmclouds()
-        return 0
-    except:
-        return 1
-
-def callndwi():
-    import modules.ndwi as n2w
-    try:
-        n2w.ndwi2watermask()
-        return 0
-    except:
-        return 1
-
+        print('please provide one of "rmclouds", "getscenes" or "ndwi"')
 
 
 if __name__ == "__main__":
