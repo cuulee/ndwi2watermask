3f137db960a1d6b9725f10ab9e169ee7a6d7b7b2
JM Delgado <jmigueldelgado@gmail.com>
2018-03-02 14:45:19 +0100

    add text for debugging

--- modules/cloudmask.py
+++ modules/cloudmask.py
@@ -114,13 +114,17 @@ def runFmaskStack(sceneJp2):
 #    fmask_sentinel2Stacked.py -a allbands.vrt -z angles.img -o cloud.img
 
 def interpolate_clouds_to_10m(file_clouds):
+    print('interpolate_clouds_to_10m:\n')
+    print('Open files, reserve memory:\n')
     dataset_clouds = rio.open(file_clouds)
     clouds = dataset_clouds.read(1)
     clouds10 = np.empty(shape=(int(round(clouds.shape[0] * 2)),
         int(round(clouds.shape[1] * 2))))
+    print('define transformation:\n')
 
     aff = dataset_clouds.transform
     newaff = Affine(aff[0] / 2, aff[1], aff[2],aff[3], aff[4] / 2, aff[5])
+    print('Reproject: \n')
     reproject(clouds, clouds10,
         src_transform = aff,
         dst_transform = newaff,
