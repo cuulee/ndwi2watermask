83be6fb20cfbc80d0e2a5b2845017813a74d9cfe
JM Delgado <jmigueldelgado@gmail.com>
2018-03-07 14:42:11 +0100

    adds criteria for excluding cloudmask, in case it is anomalous, skip it

--- modules/ndwi.py
+++ modules/ndwi.py
@@ -37,7 +37,6 @@ def ndwi_from_jp2(sceneJp2):
     scene=sceneJp2[0].split(".SAFE/GRANULE")[0]
     banddir = getBandDir(sceneJp2)
     file_clouds = banddir + '/cloud.img'
-    file_clouds
     print('debugging 1: '+banddir+'\n')
     p3 = glob.glob(banddir + '/*B03.jp2')
     p8 = glob.glob(banddir + '/*B08.jp2')
@@ -57,42 +56,47 @@ def ndwi_from_jp2(sceneJp2):
     band8 = band8.astype(float)
     print('debugging 4: band 8 opened and type set\n')
 
+    NDWI = (band3-band8)/(band3+band8)
+
+    ndwi_bool = NDWI > 0
+
+    print('potentially critical point: from boolean to int\n')
+    ndwi_int=ndwi_bool.astype('int16')
+
+
 #### no interpolation necessary because clouds are being computed in 10 m resolution
 
 #    clouds10 = interpolate_clouds_to_10m(file_clouds)
 #    except Exception, err:
 #        sys.stderr.write('ERROR: %s\n' % str(err))
-
-    dataset_clouds = rio.open(file_clouds)
-    clouds10 = dataset_clouds.read(1)
+    if(os.path.isfile(file_clouds)):
+        dataset_clouds = rio.open(file_clouds)
+        clouds10 = dataset_clouds.read(1)
 
     ## proceed only if the cloudmask is smaller than 70% of total scene
-    if(np.sum(clouds10) < 0.7*band3.shape[0]*band3.shape[1]):
+        if(np.sum(clouds10) < 0.7*band3.shape[0]*band3.shape[1]):
 
-        clouds_bool = (clouds10==2) | (clouds10==3)
+            clouds_bool = (clouds10==2) | (clouds10==3)
+            print('clouds_bool shape:\n')
+            print(clouds_bool[1:5,1:5])
+            print(clouds_bool.shape)
 
-        NDWI = (band3-band8)/(band3+band8)
+            ndwi_int[clouds_bool] = 0
+            print('ndwi_int shape:\n')
+            print(ndwi_int.shape)
+            print(ndwi_int[1:5,1:5])
 
-        print('ndwi shape:\n')
-        print(NDWI[1:5,1:5])
-        print(NDWI.shape)
-        print('clouds_bool shape:\n')
-        print(clouds_bool[1:5,1:5])
-        print(clouds_bool.shape)
+            print('debugging 8: writing out\n')
+            with rio.open(pths.s2aOut + "/" + scene + '.tif', 'w',driver='GTiff',crs=dataset3.crs,transform=dataset3.transform,height=ndwi_int.shape[0], width=ndwi_int.shape[1],count=1,dtype=np.int16) as dst:
+                dst.write(ndwi_int, 1)
 
-        ndwi_bool = NDWI > 0
-        print('ndwi_bool shape:\n')
-        print(ndwi_bool.shape)
-        print(ndwi_bool[1:5,1:5])
-
-        print('potentially critical point: from boolean to int\n')
-        ndwi_int=ndwi_bool.astype('int16')
-        ndwi_int[clouds_bool] = 0
-        print('ndwi_int shape:\n')
-        print(ndwi_int.shape)
-        print(ndwi_int[1:5,1:5])
-
-        print('debugging 8: writing out\n')
+        else:
+            print('################\n### skipping calculation of cloudmask because there must be either too many clouds of an error occurred\n##########################\n')
+            print("\nWarning: writing out watermask without cloudmask!\n")
+            with rio.open(pths.s2aOut + "/" + scene + '.tif', 'w',driver='GTiff',crs=dataset3.crs,transform=dataset3.transform,height=ndwi_int.shape[0], width=ndwi_int.shape[1],count=1,dtype=np.int16) as dst:
+                        dst.write(ndwi_int, 1)
+    else:
+        print("Warning: writing out watermask without cloudmask!\n")
         with rio.open(pths.s2aOut + "/" + scene + '.tif', 'w',driver='GTiff',crs=dataset3.crs,transform=dataset3.transform,height=ndwi_int.shape[0], width=ndwi_int.shape[1],count=1,dtype=np.int16) as dst:
             dst.write(ndwi_int, 1)
 
@@ -105,14 +109,14 @@ def ndwi2watermask():
         item=pths.s2aIn + '/' + item
         if re.search('^.*\.zip$', item):
             sceneJp2 = unzipJp2(item)
-            print('building vrt\n')
-            runGdalbuildvrt(sceneJp2)
-            print('fmask: making angles\n')
-            runFmaskMakeAngles(sceneJp2)
-            print('fmask: generating cloud mask\n')
-            runFmaskStack(sceneJp2)
-            print('fmask: finished ' + item+'\n')
-            print('creating ndwi: ' + item+'\n')
+            #print('building vrt\n')
+            #runGdalbuildvrt(sceneJp2)
+            #print('fmask: making angles\n')
+            #runFmaskMakeAngles(sceneJp2)
+            #print('fmask: generating cloud mask\n')
+            #runFmaskStack(sceneJp2)
+            #print('fmask: finished ' + item+'\n')
+            #print('creating ndwi: ' + item+'\n')
             ndwi_from_jp2(sceneJp2)
             os.remove(item)
             rmtree(item[:-4]+'.SAFE')
