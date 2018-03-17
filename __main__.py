a0565b8016de53cab468fed01b33bff945a31626
JM Delgado <jmigueldelgado@gmail.com>
2018-03-17 17:03:05 +0100

    adds some mesages

--- __main__.py
+++ __main__.py
@@ -9,18 +9,23 @@ def main():
         import modules.scenes as scenes
         scenes.getscenes()
     elif sys.argv[1]=="rmclouds":
+        print("remove clouds\n")
         import modules.cloudmask as clouds
         clouds.rmclouds()
     elif sys.argv[1]=="ndwi":
+        print("processing scene and computing ndwi\n")
         import modules.ndwi as n2w
         n2w.ndwi2watermask()
     elif sys.argv[1]=="test":
-        #### tests environment
+        print("tests environment\n")
         import modules.ndwi as n2w
         n2w.test_one_ndwi()
+    elif sys.argv[1]=="another test":
+        print("tests environment\n")
+        print("test!!!!\n")
     else:
         print('please provide one of "rmclouds", "getscenes" or "ndwi"')
 
 
 if __name__ == "__main__":
-    print main()
+    print(main())
